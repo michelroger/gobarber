31fbedff2bd27c604b71cd8385e67a2f012258e2
michel roger ribeiro pires <michelrogerribeiropires@MacBook-Pro-de-michel.local>
2019-07-23 16:02:06 -0300

    add upload1

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+
+/* Multer é utilizado no upload de arquivos */
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import User from './app/models/User';
 import UserController from './app/controllers/UserController';
+
+/* Realiza autenticação do cliente da api  */
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.get('/', async (req, res) => {
   const users = await User.findAll();
@@ -18,5 +27,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+// Rota de upload de arquivo usando a middleware de um único arquivo
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
